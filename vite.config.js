a79c4cbabac74838dd3515a5f2ee51915ec481b5
darioiubatti <darioiubatti@gmail.com>
2025-11-24 19:35:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/lib/',   // ← AGGIUNGILO
   plugins: [react()],
+})
   server: {
     allowedHosts: true
   },
