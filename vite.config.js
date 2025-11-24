9238cd90770077087f3a6073ca3e8d7f921ed78f
darioiubatti <darioiubatti@gmail.com>
2025-11-24 19:39:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/lib/',   // ← AGGIUNGILO
+  base: '/lib/',
   plugins: [react()],
-})
+
   server: {
     allowedHosts: true
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
+
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-}) 
+        '.js': 'jsx'
+      }
+    }
+  }
+})
